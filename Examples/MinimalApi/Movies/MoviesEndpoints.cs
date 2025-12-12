6c9a8c9c5c6287090c9b70864f4c69dbbffa14b4
pr0ject-me <yasynskyi.dm@gmail.com>
2025-12-12 13:14:22 -0600

    MinimalApi: -example of extracted and organized endpoints.

---
+++ Examples/MinimalApi/Movies/MoviesEndpoints.cs
@@ -0,0 +1,92 @@
+﻿namespace MinimalApi.Movies;
+
+public static class MoviesEndpoints
+{
+    public static void MapMoviesEndpoints(this WebApplication app)
+    {
+        app.MapGet("/movies", async (MoviesInMemoryRepository repository) => await repository.GetAll());
+
+        app.MapGet("/movies/{id:Guid}", async (Guid id, MoviesInMemoryRepository repository) =>
+        await repository.GetById(id) is Movie movie
+            ? Results.Ok(movie)
+            : Results.NotFound()
+        );
+
+        app.MapPost("/movies", async (Movie movie, MoviesInMemoryRepository repository) =>
+        {
+            var newMovie = movie with { Id = Guid.NewGuid() }; // with creates a copy of an instance
+
+            return await repository.Add(newMovie) is Movie createdMovie
+            ? Results.Created($"/movies/{createdMovie.Id}", createdMovie)
+            : Results.InternalServerError();
+        });
+
+        app.MapPut("/movies/{id:Guid}", async (Guid id, Movie movie, MoviesInMemoryRepository repository) =>
+        {
+            var existingMovie = await repository.GetById(id);
+            if (existingMovie is null) return Results.NotFound();
+
+            var updatedMovie = existingMovie with
+            {
+                DirectorName = movie.DirectorName,
+                ReleaseYear = movie.ReleaseYear,
+                Title = movie.Title
+            };
+
+            return Results.Ok(updatedMovie);
+
+        });
+
+        app.MapDelete("/movies/{id:Guid}", async (Guid id, MoviesInMemoryRepository repository) =>
+        {
+            var movie = await repository.GetById(id);
+            if (movie is null) return Results.NotFound();
+
+            await repository.Delete(id);
+            return Results.NoContent();
+        });
+    }
+}
+
+
+public record Movie(Guid Id, string Title, int ReleaseYear, string DirectorName); //For simplicity 1:1 relation
+
+public class MoviesInMemoryRepository
+{
+    private readonly List<Movie> _movies =
+    [
+        new Movie(Guid.NewGuid(), "Inception", 2010, "Nolan"),
+        new Movie(Guid.NewGuid(), "The Matrix", 1999, "Wachowski"),
+        new Movie(Guid.NewGuid(), "Interstellar", 2014,"Nolan"),
+    ];
+
+    //Simulate async database calls
+    //Async Await is not needed when we pass Task to a caller where it can be awaited.
+    public Task<List<Movie>> GetAll() => Task.FromResult(_movies);
+
+    public async Task<Movie?> GetById(Guid id) => await Task.FromResult(_movies.FirstOrDefault(m => m.Id == id));
+
+    public async Task<Movie?> Add(Movie movie)
+    {
+        _movies.Add(movie);
+        return await Task.FromResult(movie);
+    }
+
+    public async Task<Movie?> Update(Movie movie)
+    {
+        var index = _movies.FindIndex(x => x.Id == movie.Id);
+        if (index == -1) return default;
+
+        _movies[index] = movie;
+        return movie;
+    }
+
+    public async Task<bool> Delete(Guid id)
+    {
+        var movie = _movies.FindIndex(m => m.Id == id);
+        if (movie == -1) return await Task.FromResult(false);
+
+        _movies.RemoveAt(movie);
+        return await Task.FromResult(true);
+    }
+}
--- Examples/MinimalApi/Program.cs
+++ Examples/MinimalApi/Program.cs
@@ -1,3 +1,6 @@
+
+using MinimalApi.Movies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 //Singleton to keep sample data in memory for Scoped and Transient we'd have a new instance every request.
-builder.Services.AddSingleton<MoviesRepository>();
+builder.Services.AddSingleton<MoviesInMemoryRepository>();
 
 var app = builder.Build();
 
@@ -17,7 +20,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-#region DefaultExample
+//Register endpoints.
+app.MapMoviesEndpoints();
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -36,101 +41,11 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
-#endregion
-
-#region MoviesExample
-app.MapGet("/movies", async (MoviesRepository repository) => await repository.GetAll());
-
-app.MapGet("/movies/{id:Guid}", async (Guid id, MoviesRepository repository) =>
-await repository.GetById(id) is Movie movie
-    ? Results.Ok(movie)
-    : Results.NotFound()
-);
-
-app.MapPost("/movies", async (Movie movie, MoviesRepository repository) =>
-{
-    var newMovie = movie with { Id = Guid.NewGuid() }; // with creates a copy of an instance
-
-
-    return await repository.Add(newMovie) is Movie createdMovie
-    ? Results.Created($"/movies/{createdMovie.Id}", createdMovie)
-    : Results.InternalServerError();
-});
-
-app.MapPut("/movies/{id:Guid}", async (Guid id, Movie movie, MoviesRepository repository) =>
-{
-    var existingMovie = await repository.GetById(id);
-    if (existingMovie is null) return Results.NotFound();
-
-    var updatedMovie = existingMovie with
-    {
-        Director = movie.Director,
-        ReleaseYear = movie.ReleaseYear,
-        Title = movie.Title
-    };
-
-
-    return Results.Ok(updatedMovie);
-
-});
-
-app.MapDelete("/movies/{id:Guid}", async (Guid id, MoviesRepository repository) =>
-{
-    var movie = await repository.GetById(id);
-    if (movie is null) Results.NotFound();
-
-    await repository.Delete(id);
-    return Results.NoContent();
-});
-#endregion
 
 app.Run();
 
-#region Data
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
-internal record Movie(Guid Id, string Title, int ReleaseYear, Director Director); //For simplicity 1:1 relation
-internal record Director(string Name, string Country);
-
-internal class MoviesRepository
-{
-    private readonly List<Movie> _movies =
-    [
-        new Movie(Guid.NewGuid(), "Inception", 2010, new Director("Christopher Nolan", "UK")),
-        new Movie(Guid.NewGuid(), "The Matrix", 1999, new Director("The Wachowskis", "USA")),
-        new Movie(Guid.NewGuid(), "Interstellar", 2014, new Director("Christopher Nolan", "UK")),
-    ];
-
-    //Simulate async database calls
-    public async Task<List<Movie>> GetAll() => await Task.FromResult(_movies);
-
-    public async Task<Movie?> GetById(Guid id) => await Task.FromResult(_movies.FirstOrDefault(m => m.Id == id));
-
-    public async Task<Movie?> Add(Movie movie)
-    {
-        _movies.Add(movie);
-        return await Task.FromResult(movie);
-    }
-
-    public async Task<Movie?> Update(Movie movie)
-    {
-        var index = _movies.FindIndex(x => x.Id == movie.Id);
-        if (index == -1) return default;
-
-        _movies[index] = movie;
-        return movie;
-    }
-
-    public async Task<bool> Delete(Guid id)
-    {
-        var movie = _movies.FindIndex(m => m.Id == id);
-        if (movie == -1) return await Task.FromResult(false);
-
-        _movies.RemoveAt(movie);
-        return await Task.FromResult(true);
-    }
-}
-#endregion
